e73c353b7ad46217ad274f40d8c35d3c42d796f4
Agent007little <alekc_kg@mail.ru>
2024-03-13 13:44:11 +0300

    change settings 1

--- django_project/settings.py
+++ django_project/settings.py
@@ -19,8 +19,7 @@ SECRET_KEY = env('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env('DEBUG')
 
-ALLOWED_HOSTS = env.list('ALLOWED_HOSTS')
-
+ALLOWED_HOSTS = ['127.0.0.1', 'django-project-8q3b.onrender.com']
 
 # Application definition
 
