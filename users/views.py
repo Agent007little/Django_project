c543015619695dec78e391dafe1284e2f2cf3f0a
Agent007little <alekc_kg@mail.ru>
2024-04-19 16:34:34 +0300

    create registration

--- users/views.py
+++ users/views.py
@@ -1,5 +1,29 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+# from django.contrib.auth.forms import UserCreationForm
+from .forms import UserRegisterForm
+from django.contrib import messages
 
 
 def profile(request):
     return render(request, 'users/profile.html')
+
+
+def register(request):
+    if request.method == "POST":
+        form = UserRegisterForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            messages.success(request, f'Пользовать {username} был успешно создан!')
+            return redirect('profile')
+    else:
+        form = UserRegisterForm()
+
+    return render(
+        request,
+        'users/registration.html',
+        {
+            'title': 'Страница регистрации',
+            'form': form
+        }
+    )
